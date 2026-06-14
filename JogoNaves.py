fd99c34ba1b1f7de53217bc68ac133b0b6ed9a55
Daniela <danielarodrigues2po@gmail.com>
2026-06-14 19:31:08 -0300

    implementação tiros e inimigos com colisão interativa completa

--- JogoNaves.py
+++ JogoNaves.py
@@ -7,7 +7,7 @@ cores = [
     (255, 255, 255),
     (255, 0, 255),
     (255, 0, 0),
-    (0, 255, 255)
+    (0, 255, 255),
 ]
 
 screen_length = 800
@@ -22,50 +22,81 @@ posicao_y = 600
 
 screen = pygame.display.set_mode((screen_length,screen_height))
 
+player = pygame.Rect(posicao_x, posicao_y, 64,64)
 sprite = pygame.image.load('./imgs/NaveVerde.png').convert()
 
 running = True
 clock = pygame.time.Clock()
 
+shots = []
+can_shot = True
 
 def player_moviment(key):
 
-    global posicao_x, posicao_y
+    global can_shot
 
-    if key[pygame.K_RIGHT] and posicao_x < (screen_length - 64):
-        posicao_x += 5
-    if key[pygame.K_LEFT] and posicao_x >= 0:
-        posicao_x -= 5
-    if key[pygame.K_DOWN] and posicao_y < (screen_length - 64):
-        posicao_y += 5
-    if key[pygame.K_UP] and posicao_y >= screen_height / 2:
-        posicao_y -= 5   
-    
+    if key[pygame.K_RIGHT] and player.x < (screen_length - 64):
+        player.x += 5
+    if key[pygame.K_LEFT] and player.x >= 0:
+        player.x -= 5
+    if key[pygame.K_DOWN] and player.y < (screen_length - 64):
+        player.y += 5
+    if key[pygame.K_UP] and player.y >= screen_height / 2:
+        player.y -= 5   
+    if key[pygame.K_SPACE] and can_shot:
+        create_shot()
+        can_shot = False
+
+
+def create_shot():
+    x = player.x + 28
+    y = player.y - 48
+    shot = pygame.Rect(x, y, 8, 40)
+    shots.append(shot)
+
+def manage_shots():
+    for shot in shots:
+        pygame.draw.rect(screen, cores[2], shot)
+        shot.y -= 5
+        if shot.y == 0:
+            shots.remove(shot)
+        
+        for enemy in enemys:
+            if shot.colliderect(enemy):
+                shots.remove(shot)
+                enemys.remove(enemy)
+        
 
 def create_enemy():
     x = random.randrange(0 , screen_length - enemy_size)
-    y = random.randrange(0 , (screen_length // 2) - enemy_size)
+    y = random.randrange(0 , (screen_length // 3) - enemy_size)
     enemy = pygame.Rect(x, y, enemy_size, enemy_size)
     enemys.append(enemy)
 
 def generate_enemy():
-    cor = random.randrange(1, 3)
     for enemy in enemys:
-        pygame.draw.rect(screen, cor, enemy)
+        pygame.draw.rect(screen, cores[1], enemy)
         y = random.randrange(1,5)
         enemy.y += y
         if enemy.y == posicao_y or enemy.y == screen_height:
+            enemys.remove(enemy)
+        elif player.contains(enemy):
             enemys.remove(enemy)
     
 
 ENEMY_EVENT = pygame.USEREVENT + 1
 pygame.time.set_timer(ENEMY_EVENT, 1000)
 
+RELOAD_EVENT = pygame.USEREVENT + 2
+pygame.time.set_timer(RELOAD_EVENT, 500)
+
+
 while running:
 
-
     screen.fill(cores[0])
-    screen.blit(sprite, (posicao_x, posicao_y))
+    
+    pygame.draw.rect(screen, cores[0], player)
+    screen.blit(sprite, player)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -74,10 +105,14 @@ while running:
         elif event.type == ENEMY_EVENT:
             create_enemy()
 
+        elif event.type == RELOAD_EVENT:
+            can_shot = True
+
     key = pygame.key.get_pressed()
 
     player_moviment(key)
     generate_enemy()
+    manage_shots()
 
     pygame.display.flip()
     clock.tick(60)
