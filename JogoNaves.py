779a00b2be9def3f84c82f82022194199da430e6
Daniela <danielarodrigues2po@gmail.com>
2026-06-15 10:53:05 -0300

    cboss implement

--- JogoNaves.py
+++ JogoNaves.py
@@ -2,35 +2,55 @@ import pygame
 import random
 
 pygame.init()
+pygame.font.init()
 
+# colors creation
 cores = [
     (255, 255, 255),
     (255, 0, 255),
     (255, 0, 0),
     (0, 255, 255),
+    (0,0,0)
 ]
 
-screen_length = 800
+
+# screen size
+screen_length = 1280
 screen_height = 800
 
+# Enemy create
 enemy_speed = 3
 enemy_size = 30
 enemys = []
 
-posicao_x = 400
-posicao_y = 600
-
 screen = pygame.display.set_mode((screen_length,screen_height))
 
+# boss atributtes
+boss_x = 0
+boss_y = 0
+boss = pygame.Rect(boss_x, boss_y, 800, 256)
+boss_sprite = pygame.image.load('./imgs/Boss1.png').convert()
+boss_life = 500
+
+# player atributtes
+posicao_x = 400
+posicao_y = 600
 player = pygame.Rect(posicao_x, posicao_y, 64,64)
 sprite = pygame.image.load('./imgs/NaveVerde.png').convert()
+lifes = 3
+score = 0
+
+# Font declaration
+font = pygame.font.SysFont('Comic Sans MS', 32)
 
 running = True
 clock = pygame.time.Clock()
 
+# Shots array and variable
 shots = []
 can_shot = True
 
+# player moviment and actions
 def player_moviment(key):
 
     global can_shot
@@ -47,14 +67,17 @@ def player_moviment(key):
         create_shot()
         can_shot = False
 
-
+# create shot function
 def create_shot():
     x = player.x + 28
     y = player.y - 48
     shot = pygame.Rect(x, y, 8, 40)
     shots.append(shot)
 
+# manage shots function 
 def manage_shots():
+    global score, running, boss_life
+    
     for shot in shots:
         pygame.draw.rect(screen, cores[2], shot)
         shot.y -= 5
@@ -65,15 +88,24 @@ def manage_shots():
             if shot.colliderect(enemy):
                 shots.remove(shot)
                 enemys.remove(enemy)
-        
+                score += 1
+                print(score)
 
+        if shot.colliderect(boss):
+            bosslife -= 25
+    if lifes <= 0:
+        running = False
+        
+# create enemy function
 def create_enemy():
     x = random.randrange(0 , screen_length - enemy_size)
-    y = random.randrange(0 , (screen_length // 3) - enemy_size)
+    y = random.randrange(200 , (screen_length // 5) - enemy_size)
     enemy = pygame.Rect(x, y, enemy_size, enemy_size)
     enemys.append(enemy)
 
+# generate enemy function
 def generate_enemy():
+    global lifes 
     for enemy in enemys:
         pygame.draw.rect(screen, cores[1], enemy)
         y = random.randrange(1,5)
@@ -82,19 +114,34 @@ def generate_enemy():
             enemys.remove(enemy)
         elif player.contains(enemy):
             enemys.remove(enemy)
+            lifes -= 1
+
     
 
+# Personalizade events
 ENEMY_EVENT = pygame.USEREVENT + 1
-pygame.time.set_timer(ENEMY_EVENT, 1000)
+pygame.time.set_timer(ENEMY_EVENT, 500)
 
 RELOAD_EVENT = pygame.USEREVENT + 2
 pygame.time.set_timer(RELOAD_EVENT, 500)
 
-
 while running:
 
-    screen.fill(cores[0])
+    screen.fill(cores[4])
     
+    # Font declaration
+    player_lifes = font.render(f'Lifes: {lifes}', False, cores[0])
+    player_score = font.render(f'Score: {score}', False, cores[0])
+
+    # Drawing font
+    screen.blit(player_lifes, (50, screen_height - 50))
+    screen.blit(player_score, (screen_length - 200, screen_height - 50))
+
+    # draing boss
+    pygame.draw.rect(screen, cores[0], boss)
+    screen.blit(boss_sprite, boss)
+
+    # Drawing player
     pygame.draw.rect(screen, cores[0], player)
     screen.blit(sprite, player)
 
@@ -110,6 +157,7 @@ while running:
 
     key = pygame.key.get_pressed()
 
+    # Functions relatable to controlling Rects
     player_moviment(key)
     generate_enemy()
     manage_shots()
