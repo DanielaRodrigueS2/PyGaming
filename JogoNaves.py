9b0fffef7bc55290100d1fd9226a0c6e1e260be6
Daniela <danielarodrigues2po@gmail.com>
2026-06-16 11:40:48 -0300

    boss_special_attack

--- JogoNaves.py
+++ JogoNaves.py
@@ -11,7 +11,8 @@ cores = [
     (255, 0, 0),
     (0, 255, 255),
     (0,0,0),
-    (0, 0, 255)
+    (0, 0, 255),
+    (255, 255, 224)
 ]
 
 
@@ -20,8 +21,8 @@ screen_length = 800
 screen_height = 800
 
 # Enemy create
-enemy_speed = 3
-enemy_size = 30
+enemy_speed = 4
+enemy_size = 10
 enemys = []
 
 screen = pygame.display.set_mode((screen_length,screen_height))
@@ -33,8 +34,8 @@ boss = pygame.Rect(boss_x, boss_y, 800, 256)
 boss_sprite = pygame.image.load('./imgs/Boss1.png').convert()
 boss_life = 800
 
-# boss health bar atributtes
-# health_bar = pygame.Rect(0, screen_height - 50, boss_life, 50)
+# boss attack
+attacks = []
 
 # player atributtes
 posicao_x = 400
@@ -122,14 +123,44 @@ def generate_enemy():
             enemys.remove(enemy)
             lifes -= 1
 
+def create_boss_attack():
+    spawn = random.randrange(100, 300)
+    boss_attack1 = pygame.Rect((screen_length // 2)  + spawn, 200, 20, 200)
+    boss_attack2 = pygame.Rect((screen_length // 2) - spawn, 200, 20, 200)
+    attacks.append(boss_attack1)
+    attacks.append(boss_attack2)
+
+def boss_attack():
+
+    global boss_can_attack
+    timer = False
     
+    for attack in attacks:
+
+        pygame.draw.rect(screen, cores[6], attack)
+        pygame.draw.rect(screen, cores[6], attack)
+
+        attack.y += 5
+
+        if timer:
+            attack.x += 3
+            timer = False
+        else:
+            attack.x -= 2
+            timer = True
+
+        if attack.y >= 900:
+            attacks.remove(attack)
 
 # Personalizade events
 ENEMY_EVENT = pygame.USEREVENT + 1
-pygame.time.set_timer(ENEMY_EVENT, 500)
+pygame.time.set_timer(ENEMY_EVENT, 200)
 
 RELOAD_EVENT = pygame.USEREVENT + 2
 pygame.time.set_timer(RELOAD_EVENT, 500)
+
+BOSS_ATTACK_EVENT = pygame.USEREVENT + 3
+pygame.time.set_timer(BOSS_ATTACK_EVENT, 4000)
 
 while running:
 
@@ -165,12 +196,17 @@ while running:
         elif event.type == RELOAD_EVENT:
             can_shot = True
 
+        elif event.type == BOSS_ATTACK_EVENT:
+            create_boss_attack()
+
     key = pygame.key.get_pressed()
 
     # Functions relatable to controlling Rects
     player_moviment(key)
     generate_enemy()
     manage_shots()
+    boss_attack()
+    
 
     pygame.display.flip()
     clock.tick(60)
