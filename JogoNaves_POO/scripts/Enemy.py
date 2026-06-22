cebc1553a560bdbcf83cb30672c3fe593be9f3ae
Daniela <danielarodrigues2po@gmail.com>
2026-06-22 11:11:44 -0300

    Creation e Manipuation of enemies

--- JogoNaves_POO/scripts/Enemy.py
+++ JogoNaves_POO/scripts/Enemy.py
@@ -1,16 +1,24 @@
 import pygame as pg
 import random as rd
 
-class Enemy():
+class Enemy(pg.sprite.Sprite):
 
-    def __init__(self, size):
-        self.x = rd.randrange(0, 800 - size)
-        self.y = rd.randrange(200, 400 - size)
-        self.rect = pg.Rect(self.x, self.y, size, size)
-        self.speed = rd.randrange(1,4)
+    def __init__(self):
+        pg.sprite.Sprite.__init__(self)
+
+        self.image = pg.Surface((10,20))
+        self.image.fill((255, 0, 255))
+        self.x = rd.randrange(0, 800 - 10)
+        self.y = rd.randrange(200, 400 - 10)
+        self.rect = self.image.get_rect()
+        self.rect.center = (self.x, self.y)
+        self.speed = rd.randrange(3,5)
 
     def update(self):
         self.rect.y += self.speed
+
+        if self.rect.y >= 800:
+            self.kill
 
     def draw(self, screen):
         pg.draw.rect(screen, (255, 0, 255), self.rect)
--- JogoNaves_POO/scripts/GameScreen.py
+++ JogoNaves_POO/scripts/GameScreen.py
@@ -1,23 +1,45 @@
 from scripts.Screen import Screen
 from scripts.Player import Player
+from scripts.Enemy import Enemy
 import pygame as pg
 
+
 class GameScreen(Screen):
+
+    ENEMY_EVENT = pg.USEREVENT + 1
+    RELOAD_EVENT = pg.USEREVENT + 2
+    BOSS_ATTACK_EVENT = pg.USEREVENT + 3
+    RESET_HITBOX_EVENT = pg.USEREVENT + 4
+
     def __init__(self, game):
 
         super().__init__(game)
 
+        #Events Timer
+        pg.time.set_timer(self.ENEMY_EVENT, 200)
+        pg.time.set_timer(self.RELOAD_EVENT, 500)
+        pg.time.set_timer(self.BOSS_ATTACK_EVENT, 4000)
+        pg.time.set_timer(self.RESET_HITBOX_EVENT, 2500)
+
+        self.enemies = pg.sprite.Group()
         self.player = Player()
 
     def update(self):
         
         self.player.update()
 
+        self.enemies.update()
+
     
     def handle_events(self, event):
-        pass
+        if event.type == self.ENEMY_EVENT:
+            enemy = Enemy()
+            self.enemies.add(enemy)
         
 
     def draw(self, screen):
+
         screen.fill((50, 100, 100))
         self.player.draw(screen)
+
+        self.enemies.draw(screen)
