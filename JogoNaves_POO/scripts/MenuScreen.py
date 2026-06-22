b8ff8331d2547e40422ff17ae1d7d4b0048a5364
Daniela <danielarodrigues2po@gmail.com>
2026-06-22 08:38:19 -0300

    MenuScreen implement

--- JogoNaves_POO/scripts/MenuScreen.py
+++ JogoNaves_POO/scripts/MenuScreen.py
@@ -2,13 +2,26 @@ from Screen import Screen
 import pygame as pg
 
 class MenuScreen(Screen):
+
+    def __init__(self, game):
+        super().__init__(game)
+
+        self.font = pg.font.SysFont('Comic Sans MS', 60)
+
     def handle_events(self, event):
         if event.type == pg.KEYDOWN:
             if event.key == pg.K_RETURN:
                 self.game.change_screen("game")
 
+    def update(self):
+        pass
+
     def draw(self, surface):
         surface.fill((40,40,40))
+
+        text = self.font.render('PRESSIONE ENTER PARA INICIAR', True, (255,255,255))
+
+        surface.blit(text, (200, 180))
 
 
 
